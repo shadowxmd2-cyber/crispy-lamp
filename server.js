163b4473d543a96996e3e54851bb97053f1c8903
shadowxmd2-cyber <shadowxmd2@gmail.com>
2025-11-23 19:51:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,58 +1,19 @@
 import express from "express";
-import fetch from "node-fetch";
-import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
-app.use(cors());
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const API_KEY = "dda4ae136624f95538d55df41565bb3c4c4df5502ef008ad7259e31171df1afb";
-const BASE = "https://foreign-marna-sithaunarathnapromax-9a005c2e.koyeb.app/api/cineru";
+app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/pages"));
 
 app.get("/", (req, res) => {
-  res.send("SubMovies LK API Working on Render 🚀");
-});
-
-// SEARCH
-app.get("/api/search", async (req, res) => {
-  try {
-    const q = req.query.q;
-    if (!q) return res.json({ error: "Missing query" });
-
-    const url = `${BASE}/search?apiKey=${API_KEY}&query=${q}`;
-    const data = await fetch(url).then(r => r.json());
-    res.json(data);
-  } catch (err) {
-    res.status(500).json({ error: "Search failed", details: err.message });
-  }
-});
-
-// MOVIE DETAILS
-app.get("/api/movie", async (req, res) => {
-  try {
-    const urlParam = req.query.url;
-    if (!urlParam) return res.json({ error: "Missing movie URL" });
-
-    const url = `${BASE}/movie?apiKey=${API_KEY}&url=${urlParam}`;
-    const data = await fetch(url).then(r => r.json());
-    res.json(data);
-  } catch (err) {
-    res.status(500).json({ error: "Movie fetch failed", details: err.message });
-  }
-});
-
-// TV SHOW DETAILS
-app.get("/api/tvshow", async (req, res) => {
-  try {
-    const urlParam = req.query.url;
-    if (!urlParam) return res.json({ error: "Missing tvshow URL" });
-
-    const url = `${BASE}/tvshow?apiKey=${API_KEY}&url=${urlParam}`;
-    const data = await fetch(url).then(r => r.json());
-    res.json(data);
-  } catch (err) {
-    res.status(500).json({ error: "TV fetch failed", details: err.message });
-  }
+    res.sendFile(__dirname + "/pages/index.html");
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`API running on port ${PORT}`));
+app.listen(PORT, () =>
+    console.log(`Server running on http://localhost:${PORT}`)
+);
