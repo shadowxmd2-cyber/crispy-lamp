c173b935b123454895c3fc834c5f12b8426a7e56
shadowxmd2-cyber <shadowxmd2@gmail.com>
2025-11-23 19:50:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,1 @@
-export const API_KEY = "dda4ae136624f95538d55df41565bb3c4c4df5502ef008ad7259e31171df1afb";
-
-export const BASE_URL = "https://foreign-marna-sithaunarathnapromax-9a005c2e.koyeb.app/api/cineru";
+export const API_BASE = "https://foreign-marna-sithaunarathnapromax-9a005c2e.koyeb.app/api";
