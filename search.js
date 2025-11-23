f5aa7ee98c1d8f78afa5668d6918c90b4234d45e
shadowxmd2-cyber <shadowxmd2@gmail.com>
2025-11-23 19:57:43 +0530

    Update search.js

--- search.js
+++ search.js
@@ -1,28 +1,7 @@
-import { API_KEY, BASE_URL } from "./config.js";
+import { API_BASE } from "./config.js";
 
-const q = new URLSearchParams(location.search).get("q");
-
-document.querySelector("#search-input").value = q;
-
-async function searchMovies() {
-    const res = await fetch(`${BASE_URL}/search?apiKey=${API_KEY}&query=${q}`);
-    const data = await res.json();
-
-    const container = document.querySelector("#results");
-    container.innerHTML = "";
-
-    data.results.forEach(movie => {
-        container.innerHTML += `
-            <div class="movie-card" onclick="goMovie('${movie.url}')">
-                <img src="${movie.poster}" />
-                <p>${movie.title}</p>
-            </div>
-        `;
-    });
+async function searchMovies(q) {
+  const res = await fetch(`${API_BASE}/search?q=${q}`);
+  const data = await res.json();
+  console.log(data);
 }
-
-window.goMovie = function (url) {
-    window.location.href = `/pages/movie.html?url=${encodeURIComponent(url)}`;
-}
-
-searchMovies();
