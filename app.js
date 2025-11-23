16cb1827b86e7b7d7d7dab0fc59785cd666851f0
shadowxmd2-cyber <shadowxmd2@gmail.com>
2025-11-23 19:54:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,36 +1,8 @@
-import { API_KEY, BASE_URL } from "./config.js";
+import { API_BASE } from "./config.js";
 
-async function loadPopular() {
-    const res = await fetch(`${BASE_URL}/search?apiKey=${API_KEY}&query=popular`);
-    const data = await res.json();
-
-    displayMovies(data.results, "popular");
+async function loadMovies() {
+  const res = await fetch(`${API_BASE}/movies`);
+  const data = await res.json();
+  console.log(data);
 }
-
-async function loadLatest() {
-    const res = await fetch(`${BASE_URL}/search?apiKey=${API_KEY}&query=latest`);
-    const data = await res.json();
-
-    displayMovies(data.results, "latest");
-}
-
-function displayMovies(list, section) {
-    const container = document.querySelector(`#${section}-movies`);
-    container.innerHTML = "";
-
-    list.forEach(movie => {
-        container.innerHTML += `
-            <div class="movie-card" onclick="goMovie('${movie.url}')">
-                <img src="${movie.poster}" />
-                <p>${movie.title}</p>
-            </div>
-        `;
-    });
-}
-
-window.goMovie = function (url) {
-    window.location.href = `/pages/movie.html?url=${encodeURIComponent(url)}`;
-}
-
-loadPopular();
-loadLatest();
+loadMovies();
